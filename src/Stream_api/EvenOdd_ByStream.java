b9d30aab5e16f3b4a9e1e368141da4c84991f402
bakhade purvesh <purveshbakhade78@gmail.com>
2025-12-29 17:04:35 +0530

    Stream API basics implemented ✨

---
+++ src/Stream_api/EvenOdd_ByStream.java
@@ -0,0 +1,26 @@
+package Stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class EvenOdd_ByStream {
+
+	public static void main(String[] args) {
+		
+		
+//		List<Integer> number = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+//		number.stream().filter(num -> num%2==0).forEach(s -> System.out.println(s));
+		
+//	    int[] arr = {1,2,3,4,5,6,7,8,9,10};
+//		Arrays.stream(arr).filter(num -> num%2==1).forEach(a -> System.out.println(a));
+		
+		 
+
+		 
+		
+		
+		
+	}
+	
+	
+}
